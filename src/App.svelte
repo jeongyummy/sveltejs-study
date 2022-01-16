bc48c9c3a4298e290f4a3fdc579b9e9ef4efc53f
Yeoony <83230013+jeongyummy@users.noreply.github.com>
2022-01-16 23:54:31 +0900

    svelte 공통 컴포넌트 만들기 예제

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,12 @@
 <script>
-	let text = ''
+	import Fruits from './Fruits.svelte'
+
+	let fruits = ['Apple', 'Banana', 'Cherry', 'Orange', 'Mango']
+
 </script>
 
-<main>
-	<h1>Hello {text} !! </h1>
-	<input type="text" value={text} on:input={(e) => {text = e.target.value}} />
-	<input type="text" bind:value={text} on:input={(e) => {text = e.target.value}} /> 
-	<!-- bind 가 붙으면 양방향 바인딩, 없으면 그냥 단방향 바인딩 -->
-	<button on:click={() => {text = 'jeong'}}>
-	Click
-	</button>
-</main>
 
-<style>
-</style>
+<Fruits {fruits} />
+<Fruits {fruits} reverse />
+<Fruits {fruits} slice="-2" />
+<Fruits {fruits} slice="0, 3" />
---
+++ src/Fruits.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let fruits
+    export let reverse
+    export let slice
+
+    let computedFruits = []
+    let name = ''
+
+    if(reverse) {
+        computedFruits = [...fruits].reverse()
+        name = 'reverse'
+    } else if (slice) {
+        // 쉼표로 쪼개기
+        computedFruits = fruits.slice(...slice.split(','))
+        name = `slice ${slice}`
+    } else {
+        computedFruits = fruits
+    }
+</script>
+
+<h2>
+    Fruits
+</h2>
+<ul>
+    {#each computedFruits as fruit}
+        <li>{fruit}</li>
+    {/each}
+</ul>
