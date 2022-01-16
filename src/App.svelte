293c20b8d5364ade6a9cbb91a5ed626ac0f4cb21
Yeoony <83230013+jeongyummy@users.noreply.github.com>
2022-01-16 23:26:52 +0900

    input 이벤트 에러 수정

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,7 @@
 <main>
 	<h1>Hello {text} !! </h1>
 	<input type="text" value={text} on:input={(e) => {text = e.target.value}} />
-	<input type="text" bind:value={text} on:input={(e) => {text.e.target.value}} /> 
+	<input type="text" bind:value={text} on:input={(e) => {text = e.target.value}} /> 
 	<!-- bind 가 붙으면 양방향 바인딩, 없으면 그냥 단방향 바인딩 -->
 	<button on:click={() => {text = 'jeong'}}>
 	Click
